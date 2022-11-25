b3160dfce7f2de4ae1a30de14624522ea629fd32
jeff ndungu <ndungujeff.ke@gmail.com>
2022-11-26 01:32:41 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution in this file!
+
+const employee = {};
+employee.name = "Jeff";
+employee.streetAddress = "Nairobi";
+
+
+employee;
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value
+
+    return newEmployee
+}
+
+const nonDestructiveUpdatedEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "sam"
+    )
+
+    //adding object using dot notation
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    
+    return employee;
+}
+
+const destructiveEmployeeUpdates = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "sam"
+   )
+   console.log(destructiveEmployeeUpdates)
+
+function deleteFromEmployeeByKey(employee,key){
+    const employeeCopy = {...employee}
+    delete employeeCopy["name"]
+
+
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    const newEmployee = delete employee["name"]
+
+    return employee;
+}
